4f856142ca04c6a69f3e54c99e6daa2651aaaded
JAMES MBUVI <completed assignment>
2023-12-23 00:03:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// index.js
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with the actual name
+
+document.body.appendChild(newHeader);
+    
