0d2d66319668faa3ae252a481e6da6e1138a70d2
JAMES MBUVI <completed assignment>
2024-01-12 13:49:31 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mainElement = document.getElementById('main');
 mainElement.remove();
 
 const newHeader = document.createElement('h1');
-newHeader.id = 'victory';
+newHeader.id = 'victor';
 newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with the actual name
 
 document.body.appendChild(newHeader);
